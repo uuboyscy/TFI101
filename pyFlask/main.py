e8bd7027b083cc2830fa3ee112bbe87ef767733e
uuboyscy <u.shi@lndata.com>
2021-11-06 11:46:17 +0800

    Explain if __name__

---
+++ pyFlask/main.py
@@ -0,0 +1,4 @@
+import otherFunction
+
+print(otherFunction.add('bbb', 'aaa'))
+print(otherFunction.__name__)
---
+++ pyFlask/otherFunction.py
@@ -0,0 +1,5 @@
+def add(x, y):
+    return str(x) + str(y)
+
+if __name__ == '__main__':
+    print(add('aaa', 'bbb'))
